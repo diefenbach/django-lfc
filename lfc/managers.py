b85c880b2f9e2f383ba624da9b04d5ee5951ca2c
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-01-13 12:25:00 +0100

    Cleaned up BaseContentManger

--- lfc/managers.py
+++ lfc/managers.py
@@ -3,7 +3,8 @@ from django.db import models
 class BaseContentManager(models.Manager):
     def get_query_set(self):
         from lfc.utils.middleware import get_current_user
-        if get_current_user().is_superuser:
+        user = get_current_user()
+        if user and user.is_superuser:
             return super(BaseContentManager, self).get_query_set()
         else:
             return super(BaseContentManager, self).get_query_set().filter(active=True)
