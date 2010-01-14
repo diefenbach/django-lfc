4a1c11dd9790c593eb64ca93124c5318598fa4a3
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-01-14 13:48:26 +0100

    Removed unused tags (file, image, files, recent). They might be provided by a extra product; Added do_languages tag

--- lfc/templatetags/lfc_tags.py
+++ lfc/templatetags/lfc_tags.py
@@ -3,7 +3,9 @@ import datetime
 
 # django import
 from django import template
+from django.conf import settings
 from django.core.cache import cache
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 from django.template import Node, TemplateSyntaxError
 from django.template.loader import render_to_string
@@ -25,6 +27,40 @@ from lfc.models import BaseContent
 from lfc.models import Page
 
 register = template.Library()
+
+class LanguagesNode(Node):
+    """
+    """
+    def render(self, context):
+        lfc_context = context.get("lfc_context")
+
+        languages = []
+        for language in settings.LANGUAGES:
+            if lfc_context.has_language(language[0]):
+                is_available = True
+            else:
+                is_available = False
+
+            languages.append({
+                "code" : language[0],
+                "name" : language[1],
+                "is_available" : is_available,
+            })
+
+        context["lfc_languages"] = languages
+        return ''
+
+def do_languages(parser, token):
+    """Tag to put the contact form into context.
+    """
+    bits = token.contents.split()
+    len_bits = len(bits)
+    if len_bits != 1:
+        raise TemplateSyntaxError(_('%s tag needs no argument') % bits[0])
+
+    return LanguagesNode()
+
+register.tag('lfc_languages', do_languages)
 
 class ContactFormNode(Node):
     """Tag to put the contact form into context.
@@ -53,7 +89,7 @@ register.tag('contact_form', do_contact_form)
 
 @register.inclusion_tag('lfc/tags/tabs.html', takes_context=True)
 def tabs(context, page=None):
-    """Returns the top level pages as tabs
+    """Returns the top level pages as tabs (aka horizontal menu bar).
     """
     if page:
         page = page.get_content_object()
@@ -86,9 +122,9 @@ def tabs(context, page=None):
 
 @register.inclusion_tag('lfc/tags/scrollable.html', takes_context=True)
 def scrollable(context, tags=None, title=False, text=True, limit=5):
+    """Displays objects with given tabs as scrollable
     """
-    """
-    items = Page.objects.all()
+    items = BaseContent.objects.all()
 
     if tags:
         items = ModelTaggedItemManager().with_all(tags, items)
@@ -97,24 +133,11 @@ def scrollable(context, tags=None, title=False, text=True, limit=5):
         "items" : items,
         "title" : title,
         "text" : text,
-        "LANGUAGE_CODE" : context.get("LANGUAGE_CODE")
-    }
-
-@register.inclusion_tag('lfc/tags/recent.html', takes_context=True)
-def recent(context, title=True, text=False):
-    """
-    """
-    items = BaseContent.objects.all()
-    return {
-        "items" : items,
-        "title" : title,
-        "text" : text,
-        "LANGUAGE_CODE" : context.get("LANGUAGE_CODE")
     }
 
 @register.inclusion_tag('lfc/tags/rss.html', takes_context=True)
 def rss(context, url, limit=5):
-    """
+    """An inclusion tag which displays an rss feed.
     """
     feed = feedparser.parse(url)
 
@@ -200,7 +223,7 @@ def navigation(context, start_level=1, expand_level=0):
     }
 
 def _navigation_children(request, current_objs, obj, start_level, expand_level, level=2):
-    """
+    """Renders the children of given object as sub navigation tree.
     """
     obj = obj.get_content_object()
     temp = obj.sub_objects.filter(
@@ -240,7 +263,7 @@ def _navigation_children(request, current_objs, obj, start_level, expand_level, 
 
 @register.inclusion_tag('lfc/tags/breadcrumbs.html', takes_context=True)
 def breadcrumbs(context):
-    """
+    """Displays breadcrumbs.
     """
     obj = context.get("lfc_context")
     if obj is None:
@@ -261,15 +284,9 @@ def breadcrumbs(context):
         "objs" : objs,
     }
 
-@register.inclusion_tag('lfc/tags/files.html')
-def files(files):
-    """
-    """
-    return { "files" : files }
-
 @register.inclusion_tag('lfc/tags/page.html', takes_context=True)
 def page(context, slug, part):
-    """
+    """Displays the a part (title/text) of the page with passes slug.
     """
     request = context.get("request")
     page = lfc.utils.traverse_object(request, slug)
@@ -294,17 +311,17 @@ def objects_by_slug(context, slug):
     return { "objs" : objs }
 
 @register.inclusion_tag('lfc/tags/previous_next.html')
-def previous_next(page, sorting="creation_date"):
-    """
+def previous_next_by_date(obj):
+    """Displays previous/links by creation date for the given object.
     """
     try:
-        previous = page.get_previous_by_creation_date(parent=page.parent)
-    except (Page.DoesNotExist, IndexError):
+        previous = obj.get_previous_by_creation_date(parent=obj.parent)
+    except ObjectDoesNotExist:
         previous = None
 
     try:
-        next = page.get_next_by_creation_date(parent=page.parent)
-    except (Page.DoesNotExist, IndexError):
+        next = obj.get_next_by_creation_date(parent=obj.parent)
+    except ObjectDoesNotExist:
         next = None
 
     return {
@@ -313,11 +330,11 @@ def previous_next(page, sorting="creation_date"):
     }
 
 @register.inclusion_tag('lfc/tags/previous_next.html')
-def previous_next_by_position(page):
+def previous_next_by_position(obj):
+    """Displays previous/links by position for the given object.
     """
-    """
-    siblings = list(page.parent.get_sub_pages())
-    current_position = siblings.index(page)
+    siblings = [o.get_content_object() for o in obj.parent.get_sub_objects()]
+    current_position = siblings.index(obj)
     next_position = current_position + 1
     previous_position = current_position - 1
 
@@ -337,28 +354,4 @@ def previous_next_by_position(page):
     return {
         "previous" : previous,
         "next" : next,
-    }
-
-@register.inclusion_tag('lfc/tags/image.html', takes_context=True)
-def image(context, ord):
-    """Returns an image with given ord for the current page.
-    """
-    page = context.get("lfc_context")
-    return {
-        "image" : page.images.all()[ord-1],
-    }
-
-@register.inclusion_tag('lfc/tags/file.html', takes_context=True)
-def file(context, ord):
-    """Returns an file with given ord for the current page.
-    """
-    files = context.get("files")
-
-    try:
-        file = files[ord-1]
-    except IndexError:
-        file = None
-
-    return {
-        "file" : file,
     }
