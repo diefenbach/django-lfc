b844b5ea9f7df47a9c000699b6b2636fa16a20cd
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-04-15 22:17:27 +0200

    Improvement: Return correct language for using within links

--- lfc/context_processors.py
+++ lfc/context_processors.py
@@ -8,11 +8,18 @@ def main(request):
     """
     current_language = translation.get_language()
     default_language = settings.LANGUAGE_CODE
-
+    
+    is_default_language = default_language == current_language
+    if current_language == "0" or is_default_language:
+        link_language = ""
+    else:
+        link_language = current_language
+    
     return {
         "PORTAL" : lfc.utils.get_portal(),
         "LFC_MULTILANGUAGE"   : settings.LFC_MULTILANGUAGE,
         "DEFAULT_LANGUAGE"    : default_language,
         "CURRENT_LANGUAGE"    : current_language,
-        "IS_DEFAULT_LANGUAGE" : default_language == current_language,
+        "IS_DEFAULT_LANGUAGE" : is_default_language,
+        "LINK_LANGUAGE"       : link_language, 
     }
