81841d5e44171f612e788657560c5ff5a26dd67a
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-05-15 09:30:27 +0200

    Factored out initial registering to scripts. issue #1

--- lfc/models.py
+++ lfc/models.py
@@ -1070,28 +1070,3 @@ class TextPortletForm(forms.ModelForm):
     """
     class Meta:
         model = TextPortlet
-
-def register(sender, **kwargs):
-    # Portlets
-    register_portlet(NavigationPortlet, "Navigation")
-    register_portlet(PagesPortlet, "Pages")
-    register_portlet(RandomPortlet, "Random")
-    register_portlet(TextPortlet, "Text")
-
-    # Register Templates
-    from lfc.utils.registration import register_template
-    register_template(name = "Plain", path="lfc/templates/plain.html")
-    register_template(name = "Article", path="lfc/templates/article.html")
-    register_template(name = "Gallery", path="lfc/templates/gallery.html", images_columns=3)
-    register_template(name = "Overview", path="lfc/templates/overview.html")
-
-    # Content Types
-    from lfc.utils.registration import register_content_type
-    register_content_type(
-        Page,
-        name="Page",
-        sub_types=["Page"],
-        templates=["Article", "Plain", "Gallery", "Overview"],
-        default_template="Article")
-
-post_syncdb.connect(register)
