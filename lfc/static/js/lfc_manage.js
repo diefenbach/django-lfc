d674c37a0f8f0d7a72fabfd59ca3e03bac84502f
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-07-23 16:02:52 +0200

    Separated lfc related tinymce javascript from lfc manage javascript. Issue: #23

--- lfc/static/js/lfc_manage.js
+++ lfc/static/js/lfc_manage.js
@@ -1,46 +1,3 @@
-function CustomFileBrowser(field_name, url, type, win) {
-
-    url = "/manage/filebrowser?obj_id=" + $("#obj-id").attr("data"),
-    url = url + "&type=" + type;
-
-    tinyMCE.activeEditor.windowManager.open({
-        file: url,
-        width: 820,  // Your dimensions may differ - toy around with them!
-        height: 500,
-        resizable: "yes",
-        scrollbars: "yes",
-        inline: "no",
-        close_previous: "no"
-    }, {
-        window: win,
-        input: field_name,
-        editor_id: tinyMCE.selectedInstance.editorId
-    });
-    return false;
-}
-
-tinyMCE.init({
-    mode: "none",
-    theme : "advanced",
-    height : "400",
-    tab_focus : ":prev,:next",
-    button_tile_map : true,
-    plugins : "advimage, safari, fullscreen",
-    convert_urls : false,
-    theme_advanced_buttons1 : "bold, italic, underline, |, justifyleft," +
-                              "justifycenter, justifyright, justifyfull, |," +
-                              "bullist,numlist, |, outdent, indent, |, image, |, undo," +
-                              "redo, |, code, link, unlink, styleselect, formatselect, |," +
-                              "removeformat, fullscreen",
-    theme_advanced_buttons2: "",
-    theme_advanced_buttons3: "",
-    theme_advanced_buttons4: "",
-    theme_advanced_toolbar_location : "top",
-    theme_advanced_toolbar_align : "left",
-    content_css : "/media/tiny.css",
-    file_browser_callback: "CustomFileBrowser"
-})
-
 $(function() {
 
     // Message
@@ -56,19 +13,6 @@ $(function() {
         speed: "fast",
         delay: "200"
     });
-
-    // //  Dialog
-    // $("#dialog").dialog({
-    //     autoOpen: false,
-    //     closeOnEscape: true,
-    //     modal: true,
-    //     width: 800,
-    //     height: 600,
-    //     overlay: {
-    //         opacity: 0.7,
-    //         background: "black"
-    //     }
-    // });
 
     // Tabs
     $('#manage-tabs > ul').tabs({ cookie: { expires: 30 } });
