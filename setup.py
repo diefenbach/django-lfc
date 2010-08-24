9034523612f1c280d190d47b15cada1dc20a8891
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-08-24 08:05:59 +0200

    Changed Development Status to Production/Stable

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='django-lfc',
       description='A CMS based on Django.',
       long_description=README,
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Environment :: Web Environment',
           'Framework :: Django',
           'License :: OSI Approved :: BSD License',
