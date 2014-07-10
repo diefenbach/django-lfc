6e81fa4252b458172f533a700d642da3224036f1
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-07-10 21:56:38 +0200

    Adds copying of belonging images/files/portets/translations when an object is copied.

--- lfc/manage/views.py
+++ lfc/manage/views.py
@@ -5353,6 +5353,11 @@ def _paste(request, obj):
             except IntegrityError:
                 pass
 
+            _copy_images(source_obj, target_obj)
+            _copy_files(source_obj, target_obj)
+            _copy_portlets(source_obj, target_obj)
+            _copy_translations(source_obj, target_obj)
+
             # Prevent recursion
             if target not in descendants and target != source_obj:
                 _copy_descendants(source_obj, target_obj)
